7da378f91f457e46dde9309f28ecf557241753c3
kenneth-fullstack <kenneth@fullstack.ph>
2022-02-11 17:17:35 +0800

    Second Commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,58 +1,93 @@
+// import NextAuth from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+
+// export default NextAuth({
+//   // Configure one or more authentication providers
+//   providers: [
+//     CredentialsProvider({
+//       id: "domain-login",
+//       name: "Credential Account",
+//       credentials: {
+//         username: {
+//           label: "Email",
+//           type: "email",
+//         },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         if (
+//           credentials.username === "kenneth@sample.com" &&
+//           credentials.password === "admin!123"
+//         ) {
+//           const user = {
+//             /* add function to get user */
+//             id: 1,
+//             name: "Kenneth",
+//             email: "kenneth@sample.com",
+//           };
+//           return user;
+//         }
+
+//         return null;
+//       },
+//     }),
+//   ],
+
+//   callbacks: {
+//     jwt: async ({ token, user }) => {
+//       // first time jwt callback is run, user object is availables
+//       if (user) {
+//         token.id = user.id;
+//       }
+
+//       return token;
+//     },
+//     session: async ({ session, token }) => {
+//       if (token) {
+//         session.id = token.id;
+//       }
+
+//       return session;
+//     },
+//   },
+//   secret: "test",
+//   jwt: {
+//     secret: "test",
+//     encryption: true,
+//   },
+// });
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { FirebaseAdapter } from "@next-auth/firebase-adapter";
 
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firestore = (
+  firebase.apps[0] ?? firebase.initializeApp(firebaseConfig)
+).firestore();
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
 export default NextAuth({
-  // Configure one or more authentication providers
+  // https://next-auth.js.org/providers/overview
   providers: [
-    CredentialsProvider({
-      id: "domain-login",
-      name: "Credential Account",
-      credentials: {
-        username: {
-          label: "Email",
-          type: "email",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (
-          credentials.username === "kenneth@sample.com" &&
-          credentials.password === "admin!123"
-        ) {
-          const user = {
-            /* add function to get user */
-            id: 1,
-            name: "Kenneth",
-            email: "kenneth@sample.com",
-          };
-          return user;
-        }
-
-        return null;
-      },
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-
-  callbacks: {
-    jwt: async ({ token, user }) => {
-      // first time jwt callback is run, user object is availables
-      if (user) {
-        token.id = user.id;
-      }
-
-      return token;
-    },
-    session: async ({ session, token }) => {
-      if (token) {
-        session.id = token.id;
-      }
-
-      return session;
-    },
-  },
-  secret: "test",
-  jwt: {
-    secret: "test",
-    encryption: true,
-  },
+  adapter: FirebaseAdapter(firestore),
 });
