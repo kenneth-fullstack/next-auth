8d709442e49777dca67270b989f5caefb19a1751
djkenneth <dj.kenneth.pineda@gmail.com>
2022-02-12 21:07:44 +0800

    Added header -  carousel

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import "../styles/globals.scss";
-import { SessionProvider } from "next-auth/react";
+// import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+// function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+//   return (
+//     <SessionProvider session={session}>
+//       <Component {...pageProps} />
+//     </SessionProvider>
+//   );
+// }
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -57,35 +57,35 @@
 //   },
 // });
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import { FirebaseAdapter } from "@next-auth/firebase-adapter";
 
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+// import firebase from "firebase/app";
+// import "firebase/firestore";
 
-import firebase from "firebase/app";
-import "firebase/firestore";
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
 
-const firestore = (firebase.apps[0] ?? firebase.initializeApp(firebaseConfig)).firestore();
+// const firestore = (firebase.apps[0] ?? firebase.initializeApp(firebaseConfig)).firestore();
 
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
-export default NextAuth({
-  // https://next-auth.js.org/providers/overview
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-  ],
-  adapter: FirebaseAdapter(firestore),
-});
+// // For more information on each option (and a full list of options) go to
+// // https://next-auth.js.org/configuration/options
+// export default NextAuth({
+//   // https://next-auth.js.org/providers/overview
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_SECRET,
+//     }),
+//   ],
+//   adapter: FirebaseAdapter(firestore),
+// });
